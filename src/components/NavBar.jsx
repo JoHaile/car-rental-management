508159720c5fed5cbb4720b75da0034112d42fdf
John <yohannes.h93@gmail.com>
2025-06-05 02:28:14 +0300

    fix the nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,52 +1,46 @@
-import {
-  Link,
-  HStack,
-  Image,
-  Button,
-  Box,
-  Text,
-  Stack,
-  Center,
-} from "@chakra-ui/react";
+import { HStack, Image, Button, Box, Center } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import { BiChevronDown } from "react-icons/bi";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <HStack
-      display="flex"
-      justifyContent="space-between"
-      position="absolute"
-      width="100%"
-      top="2rem"
-      padding="0 2.5vw"
-    >
-      <Image src={logo} height="40px" />
+    <Center>
+      <HStack
+        display="flex"
+        justifyContent="space-between"
+        position="absolute"
+        top="2rem"
+        width="100vw"
+        maxWidth="1300px"
+        padding="0 2.5vw"
+      >
+        <Image src={logo} height="40px" />
 
-      <Box display="flex" gap="1rem">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/fleets">Fleets</NavLink>
-        <NavLink to="/about">About Us</NavLink>
+        <Box display="flex" gap="1rem">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/fleets">Fleets</NavLink>
+          <NavLink to="/about">About Us</NavLink>
 
-        {/* make it on hover to display  */}
-        <div className="menu">
-          <NavLink className="drop-link">
-            Company <BiChevronDown />
-          </NavLink>
+          {/* make it on hover to display  */}
+          <div className="menu">
+            <NavLink className="drop-link">
+              Company <BiChevronDown />
+            </NavLink>
 
-          <div className="drop-menu">
-            <NavLink className="drop-link">Rental Policy</NavLink>
-            <NavLink className="drop-link">Rental Requirement</NavLink>
-            <NavLink className="drop-link">Our Address</NavLink>
+            <div className="drop-menu">
+              <NavLink className="drop-link">Rental Policy</NavLink>
+              <NavLink className="drop-link">Rental Requirement</NavLink>
+              <NavLink className="drop-link">Our Address</NavLink>
+            </div>
           </div>
-        </div>
 
-        <NavLink to="/faq">FAQs</NavLink>
-      </Box>
+          <NavLink to="/faq">FAQs</NavLink>
+        </Box>
 
-      <Button rounded="l1">Contact Us</Button>
-    </HStack>
+        <Button rounded="l1">Contact Us</Button>
+      </HStack>
+    </Center>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import "./styles/normalize.css";
 import "./styles/index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.jsx";
