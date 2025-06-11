756b054da7393b1973d9cb1f78ec69a71b69ef1c
John <yohannes.h93@gmail.com>
2025-06-12 01:21:26 +0300

    added the json files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import Features from "../components/Features";
 import Hero from "../components/Hero";
 import HorizontalScroll from "../components/horizontal-scroll/HorizontalScroll";
 import Mission from "../components/Mission";
-import ScrollTop from "../components/scrollTop";
 import Testimonial from "../components/testimonial";
 
 function HomePage() {
