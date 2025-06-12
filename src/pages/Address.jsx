9056d304f30d187394fdaaf8efb6cc41bd26b9bc
John <yohannes.h93@gmail.com>
2025-06-12 06:03:12 +0300

    create address section with google map embedded

--- src/pages/Address.jsx
+++ src/pages/Address.jsx
@@ -1,14 +1,37 @@
-import { SimpleGrid, Box } from "@chakra-ui/react";
+import { SimpleGrid, Box, Heading } from "@chakra-ui/react";
+import data from "../assets/db.json";
+import GoogleMap from "../components/GoogleMap";
 
 function Address() {
   return (
     <div>
-      Address Page
-      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} p={5}>
-        <Box bg="blue.900" p={5} borderRadius="md" />
-        <Box bg="blue.700" p={5} borderRadius="md" />
-        <Box bg="blue.400" p={5} borderRadius="md" />
+      <Heading
+        as="h1"
+        size={{ base: "2xl", md: "3xl", lg: "4xl" }}
+        textAlign="center"
+        my={5}
+      >
+        {data.rentalCompany.name}
+      </Heading>
+      <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3 }}
+        // spacing={10}
+        p={5}
+        textAlign="center"
+        gap={5}
+      >
+        {data.rentalCompany.branches.map((brach) => (
+          <Box key={brach.branchId} p={5} borderRadius="md" boxShadow="2xl">
+            <Heading color="royalblue">{brach.city}</Heading>
+            <p>{brach.address}</p>
+            <p>{brach.phone}</p>
+            <strong style={{ color: "royalblue" }}>Opening Hours</strong>
+            <p>{brach.openingHours}</p>
+          </Box>
+        ))}
       </SimpleGrid>
+
+      <GoogleMap />
     </div>
   );
 }
