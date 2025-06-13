d5d88fc7c70e691fa43c09389d0c040ac7594406
John <yohannes.h93@gmail.com>
2025-06-14 01:31:38 +0300

    added a bunch of sorting and filtering functionalities

---
+++ src/hooks/filterCars.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import data from "../assets/db.json";
+
+const filterCars = () => {
+  const [filter, setFilter] = useState({
+    gas: false,
+    electric: false,
+    hybrid: false,
+    price: false,
+  });
+  const price = priceFilter(data);
+  const filterUser = typeFilter(price);
+
+  function priceFilter(carData) {
+    const newCars = filter.price
+      ? carData.cars.sort((car1, car2) =>
+          car1.rentalPricePerDay > car2.rentalPricePerDay ? 1 : -1
+        )
+      : carData.cars.sort((car1, car2) =>
+          car1.rentalPricePerDay > car2.rentalPricePerDay ? -1 : 1
+        );
+    // console.log(newCars);
+
+    return newCars;
+  }
+
+  function typeFilter(carData) {
+    const newCars = filter.gas
+      ? carData.filter((car) => car.features.fuelType === "Gasoline")
+      : filter.electric
+        ? carData.filter((car) => car.features.fuelType === "Electric")
+        : filter.hybrid
+          ? carData.filter((car) => car.features.fuelType === "Hybrid")
+          : carData;
+
+    return newCars;
+  }
+
+  return { filter, setFilter, filterUser };
+};
+
+export default filterCars;
--- src/pages/Fleets.jsx
+++ src/pages/Fleets.jsx
@@ -6,12 +6,16 @@ import {
   GridItem,
   Heading,
   SimpleGrid,
+  Button,
+  Spacer,
 } from "@chakra-ui/react";
 import image from "../assets/img/wide-selection.webp";
-import data from "../assets/db.json";
 import CarsCard from "../components/CarsCard";
+import filterCars from "../hooks/filterCars";
 
 function Fleets() {
+  const { filterUser, setFilter, filter } = filterCars();
+
   return (
     <>
       <Box
@@ -37,16 +41,80 @@ function Fleets() {
 
       <Grid gridTemplateColumns="repeat(6, 1fr)">
         <GridItem bgColor="red.600" colSpan={1}>
-          side bar
+          <Button
+            onClick={() =>
+              setFilter({
+                ...filter,
+                gas: !filter.gas,
+                electric: false,
+                hybrid: false,
+              })
+            }
+          >
+            Gas
+          </Button>
+          <Button
+            onClick={() =>
+              setFilter({
+                ...filter,
+                electric: !filter.electric,
+                gas: false,
+                hybrid: false,
+              })
+            }
+          >
+            Electric
+          </Button>
+          <Button
+            onClick={() =>
+              setFilter({
+                ...filter,
+                hybrid: !filter.hybrid,
+                gas: false,
+                electric: false,
+              })
+            }
+          >
+            Hybrid
+          </Button>
         </GridItem>
 
         <GridItem colSpan={5} p={{ base: ".5rem", md: "1rem" }}>
+          <Box display="flex" justifyContent="space-between" pb={"1rem"}>
+            <Heading size={{ base: "1xl", lg: "2xl" }}>
+              Filtered By
+              {filter.gas ? (
+                <span className="active"> Gasoline</span>
+              ) : filter.electric ? (
+                <span className="active"> Electric</span>
+              ) : filter.hybrid ? (
+                <span className="active"> Hybrid</span>
+              ) : filter.price ? (
+                <span className="active"> Low Price</span>
+              ) : (
+                " Relevance"
+              )}
+            </Heading>
+
+            <Button
+              onClick={() =>
+                setFilter({
+                  ...filter,
+                  price: !filter.price,
+                  // gas: false,
+                  // electric: false,
+                })
+              }
+            >
+              Price
+            </Button>
+          </Box>
           <SimpleGrid
             minChildWidth={{ base: "250px", md: "300px" }}
             gap="1rem"
             justifyItems="center"
           >
-            {data.cars.map((car) => (
+            {filterUser.map((car) => (
               <CarsCard cars={car} key={car.id} />
             ))}
           </SimpleGrid>
