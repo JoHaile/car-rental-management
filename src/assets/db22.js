d1f8f167ef8026e5c5ebd44b12e5cae2b0d31303
John <yohannes.h93@gmail.com>
2025-06-14 04:53:26 +0300

    added google map section for each branch

--- src/assets/db22.js
+++ src/assets/db22.js
@@ -313,6 +313,7 @@ export default {
         address: "12 Kazanchis St., Gondar",
         phone: "+251-58-123-4567",
         openingHours: "8:00 AM - 8:00 PM",
+        map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31149.615698551654!2d37.43513236689505!3d12.60188351130028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x16432903deb758ed%3A0x69a1df405bb22ef2!2sAutomotive%20service!5e0!3m2!1sen!2set!4v1749694924268!5m2!1sen!2set",
       },
       {
         branchId: "BAH-002",
@@ -320,6 +321,7 @@ export default {
         address: "45 Blue Nile Rd., Bahrdar",
         phone: "+251-58-765-4321",
         openingHours: "7:00 AM - 9:00 PM",
+        map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11760.168378070357!2d37.39821344781037!3d11.596307786744811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1644cf70f0966745%3A0xd11c9ac1d1f1b092!2sBahirdar%20Car%20Rental!5e0!3m2!1sen!2set!4v1749696394465!5m2!1sen!2set",
       },
       {
         branchId: "ADD-003",
@@ -327,6 +329,7 @@ export default {
         address: "89 Churchill Ave., Addis Ababa",
         phone: "+251-11-234-5678",
         openingHours: "9:00 AM - 7:00 PM",
+        map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11369.715281467168!2d38.7169569082821!3d8.965887262424284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b86ac5b1a40f3%3A0x9654fdc325137c1a!2sSuzuki%20Automotive%20Service%20plc!5e0!3m2!1sen!2set!4v1749696453320!5m2!1sen!2set",
       },
     ],
   },
